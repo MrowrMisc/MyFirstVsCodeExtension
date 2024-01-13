48df96eef031991218fbcd02003b09060dfc4c2a
Mrowr Purr <mrowr.purr@gmail.com>
2024-01-13 10:14:44 -0800

    Add some items to the test explorer

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import { GutterDecorationProvider } from './gutterDecorationProvider';
 import { LensCodeLensProvider } from './lensCodeLensProvider';
 
+function runAllTests() {}
+
 export function activate(context: vscode.ExtensionContext) {
     const gutterProvider = new GutterDecorationProvider(context);
     context.subscriptions.push(vscode.languages.registerCodeLensProvider({ language: 'typescript' }, new LensCodeLensProvider()));
@@ -12,6 +14,22 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from Specs.cpp and MiniSpecs.cpp Test Runner!');
 	});
 	context.subscriptions.push(disposable);
+
+	const testController = vscode.tests.createTestController('specs-cpp', 'Specs.cpp Test Runner');
+	context.subscriptions.push(testController);
+
+	testController.createRunProfile('All Tests', vscode.TestRunProfileKind.Run, runAllTests);
+	testController.createRunProfile('Debug All Tests', vscode.TestRunProfileKind.Debug, runAllTests);
+	testController.createRunProfile('Current File', vscode.TestRunProfileKind.Run, runAllTests);
+	testController.createRunProfile('Debug Current File', vscode.TestRunProfileKind.Debug, runAllTests);
+
+	const test1 = testController.createTestItem('test-id-1', 'Test 1', vscode.Uri.file('/path/to/test-1.cpp'));
+	const test2 = testController.createTestItem('test-id-2', 'Test 2', vscode.Uri.file('/path/to/test-2.cpp'));
+	const test3 = testController.createTestItem('test-id-3', 'Test 3', vscode.Uri.file('/path/to/test-3.cpp'));
+
+	testController.items.add(test1);
+	testController.items.add(test2);
+	testController.items.add(test3);
 }
 
 export function deactivate() {}
